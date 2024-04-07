02f98b184c1753599d2c2d0bb403de988a7df0b6
Cayo Legal <cayolegal@hotmail.com>
2024-04-07 18:41:51 -0400

    add comment

--- src/constants/validations.constants.ts
+++ src/constants/validations.constants.ts
@@ -1,3 +1,5 @@
+/* This messages targets to the i18n translation values, if you don't want to use it just change the values */
+
 export const requiredMessage = "required_message";
 export const invalidEmailMessage = "invalid_email_message";
 export const invalidIdMessage = "invalid_id_message";
