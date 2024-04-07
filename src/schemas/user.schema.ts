cb52f0d57b5a4c39cb6aaecbd909ba6d17c2a89c
Cayo Legal <cayolegal@hotmail.com>
2024-04-07 02:45:14 -0400

    add name validation

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -2,7 +2,7 @@ import * as Yup from "yup";
 import { requiredMessage, invalidEmailMessage } from "src/constants";
 
 export const userSchema = Yup.object().shape({
-  name: Yup.string().required(requiredMessage),
+  name: Yup.string().required(requiredMessage).min(3, "El nombre debe ser válido"),
   email: Yup.string().email(invalidEmailMessage).required(requiredMessage),
   password: Yup.string().required(requiredMessage),
 });
