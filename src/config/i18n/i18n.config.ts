80f9e21a28477597f5e48b5f8d27a98af2ebe451
Cayo Legal <cayolegal@hotmail.com>
2024-04-07 17:56:26 -0400

    beauty code

--- src/config/i18n/i18n.config.ts
+++ src/config/i18n/i18n.config.ts
@@ -3,10 +3,11 @@ import path from "path";
 export const i18n = i18nextLib;
 
 i18n.configure({
-  locales: ['en', 'es'],
+  defaultLocale: "es",
   directory: path.join(__dirname, 'locales'),
-  header: "lang",
-  queryParameter: "lang",
   extension: ".json",
-  parser: JSON
+  header: "lang",
+  locales: ['en', 'es'],
+  parser: JSON,
+  queryParameter: "lang",
 });
--- src/constants/validations.constants.ts
+++ src/constants/validations.constants.ts
@@ -1,3 +1,3 @@
-export const requiredMessage = "required_message";
+ export const requiredMessage = "required_message";
 export const invalidEmailMessage = "invalid_email_message";
 export const invalidIdMessage = "invalid_id_message";
