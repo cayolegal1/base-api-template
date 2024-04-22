de1f1b873583e60742d93c1acd2fefd3210b214d
Cayo Legal <cayolegal@hotmail.com>
2024-04-21 23:42:52 -0400

    add multiple schemas checker fn

--- src/middlewares/schema.handler.ts
+++ src/middlewares/schema.handler.ts
@@ -1,6 +1,11 @@
 import type { Request, Response, NextFunction, } from "express";
 import type { ObjectSchema } from "yup";
 
+type SchemaHandler <T>= {
+  schema: ObjectSchema<T>,
+  property: keyof Request<T>,
+}
+
 export const checkSchema = <T>(
   schema: ObjectSchema<T>,
   property: keyof Request<T>,
@@ -13,6 +18,20 @@ export const checkSchema = <T>(
     try {
       const data = req[property];
       await schema.validate(data, { abortEarly: false });
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+
+export const checkSchemas = <T>(handlers: SchemaHandler<T>[]) => {
+  return async (req: Request<T>, _response: Response, next: NextFunction) => {
+    try {
+      for (const handler of handlers) {
+        checkSchema(handler.schema, handler.property);
+      }
+
       next();
     } catch (error) {
       next(error);
