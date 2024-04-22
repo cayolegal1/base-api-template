e7aff25c76d8901bec67af61bf39996949a204ce
Cayo Legal <cayolegal@hotmail.com>
2024-04-22 00:07:21 -0400

    format code

--- src/utils/errors/base-error.ts
+++ src/utils/errors/base-error.ts
@@ -1,19 +1,24 @@
 import type { ErrorConstructor } from "@custom-types/error.type";
 
 export class CustomError extends Error {
+  
   name: string;
   statusCode: number;
   internalMessage?: string;
+
   constructor({
     message = "Internal server error",
     statusCode = 500,
     name,
     internalMessage,
   }: ErrorConstructor = {}) {
+
     super(message);
+
     Object.assign(this, { name, statusCode, internalMessage });
     if (Error.captureStackTrace) {
       Error.captureStackTrace(this, this.constructor);
     }
+
   }
 }
