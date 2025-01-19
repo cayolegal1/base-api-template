072f261f6d63474565ce91b8908dc98f92a2e323
Cayo Legal <cayolegal@hotmail.com>
2025-01-19 02:02:11 -0300

    code refactor

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,50 +1,41 @@
 import express from "express";
 import passport from "passport";
 import { checkSchema, checkRoleOrPermission } from "@middlewares/index";
-import { userSchema, getUserSchema } from "@schemas/index";
+import { userSchema, userByIdSchema } from "@schemas/index";
 import { STATUS_CODE } from "src/utils/constants";
 
 const router = express.Router();
 
-router.get(
-  "/:id",
-  checkSchema(getUserSchema, "params"),
-  (req, res, next) => {
-    try {
-      const { id } = req.params;
-      res.status(STATUS_CODE.OK).json({ success: true, id });
-    } catch (error) {
-      next(error);
-    }
-  },
-);
+router.get("/:id", checkSchema(userByIdSchema, "params"), (req, res, next) => {
+  try {
+    const { id } = req.params;
+    res.status(STATUS_CODE.OK).json({ success: true, id });
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post(
-  "/create",
-  checkSchema(userSchema, "body"),
-  (req, res, next) => {
-    try {
-      const body = req.body;
-      res.status(STATUS_CODE.CREATED).json({ success: true, body });
-    } catch (error) {
-      next(error);
-    }
-  },
-);
-
+router.post("/create", checkSchema(userSchema, "body"), (req, res, next) => {
+  try {
+    const body = req.body;
+    res.status(STATUS_CODE.CREATED).json({ success: true, body });
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.patch(
   "/update/:id",
   passport.authenticate("jwt", { session: false }),
   checkRoleOrPermission("admin"),
-  checkSchema(getUserSchema, "params"),
+  checkSchema(userByIdSchema, "params"),
   checkSchema(userSchema, "body"),
   (req, res, next) => {
     try {
       const body = req.body;
       res.status(STATUS_CODE.OK).json({ success: true, body });
     } catch (error) {
-      next(error)
+      next(error);
     }
   },
 );
@@ -53,7 +44,7 @@ router.delete(
   "/delete/:id",
   passport.authenticate("jwt", { session: false }),
   checkRoleOrPermission("admin"),
-  checkSchema(getUserSchema, "params"),
+  checkSchema(userByIdSchema, "params"),
   (req, res, next) => {
     try {
       const { id } = req.params;
@@ -65,5 +56,3 @@ router.delete(
 );
 
 export default router;
-
-
--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -5,18 +5,20 @@ import {
   invalidIdMessage,
   invalidNameMessage,
   invalidRolMessage,
-  userRoles
+  userRoles,
 } from "src/utils/constants";
 
 export const userSchema = Yup.object().shape({
   id: Yup.string().nullable(),
   name: Yup.string().required(requiredMessage).min(3, invalidNameMessage),
-  role: Yup.string().oneOf(userRoles, invalidRolMessage).required(requiredMessage),
+  role: Yup.string()
+    .oneOf(userRoles, invalidRolMessage)
+    .required(requiredMessage),
   email: Yup.string().email(invalidEmailMessage).required(requiredMessage),
   password: Yup.string().required(requiredMessage),
 });
 
-export const getUserSchema = Yup.object().shape({
+export const userByIdSchema = Yup.object().shape({
   id: Yup.string()
     .required(requiredMessage)
     .test({
